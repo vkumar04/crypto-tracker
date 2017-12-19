08b2e942cd2e16197d78f9305903d2896612ec81
Vick Kumar <vick.kumar44@gmail.com>
2017-12-19 15:18:17 -0500

    rewrote everything with a better api

--- tracker.py
+++ tracker.py
@@ -1,26 +1,15 @@
 import requests
 import time
 
-url = 'https://bittrex.com/api/v1.1/public/getmarketsummaries'
-r = requests.get(url)
-response_dict = r.json()
-results = response_dict['result']
-
 def getData():
-  for result in results:
-    if result['MarketName'] == "BTC-LTC":
-      ltc = result
-    elif result['MarketName'] == "USDT-BTC":
-      btc = result
-    elif result['MarketName'] == "BTC-ETH":
-      eth = result
-    elif result['MarketName'] == "USDT-XRP":
-      xrp = result
-  print (ltc['MarketName'], "last:", ltc['Last'])
-  print (btc['MarketName'], "last:", btc['Last'])
-  print (eth['MarketName'], "last:", eth['Last'])
-  print (xrp['MarketName'], "last:", xrp['Last'])
-  print ('\n')
+  url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP,XLM&tsyms=USD'
+  r = requests.get(url)
+  response_dict = r.json()
+  for crypto in response_dict:
+    print(crypto, response_dict[crypto])
+    print('\n')
+
 while True:
   getData()
-  time.sleep(30)
+  time.sleep(5)
+
